095b159655260dd109cbdbeddcc316bb4b14cc87
John <jjacobson15@huskers.unl.edu>
2023-08-24 11:21:54 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: John Jacobson
+ * Partner: Jay Jay
+ * Date: 2023/08/24
+ * Purpose: Prints Name
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
